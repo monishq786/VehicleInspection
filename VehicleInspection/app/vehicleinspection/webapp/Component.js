4a4c2535345a71238525a77df8e1b5207d62405e
Moh Monish <mohdmonish8882@gmail.com>
2025-02-11 10:45:34 +0000

    changes for generic

--- VehicleInspection/app/vehicleinspection/webapp/Component.js
+++ VehicleInspection/app/vehicleinspection/webapp/Component.js
@@ -1,26 +1,26 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "adnoc/vi/vehicleinspection/model/models"
+    "adnoc/vi/vehicleinspection/model/models",
+    "sap/ui/model/json/JSONModel"
 ], (UIComponent, models) => {
     "use strict";
 
     return UIComponent.extend("adnoc.vi.vehicleinspection.Component", {
         metadata: {
-            manifest: "json",
-            interfaces: [
-                "sap.ui.core.IAsyncContentCreation"
-            ]
+            manifest: "json"
         },
 
         init() {
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
+            // enable routing
+            this.getRouter().initialize();
+
             // set the device model
             this.setModel(models.createDeviceModel(), "device");
 
-            // enable routing
-            this.getRouter().initialize();
+            
         }
     });
 });
