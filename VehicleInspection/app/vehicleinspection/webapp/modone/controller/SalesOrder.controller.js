fbe802ef8b1d904089464bcfd5de68354b3f032b
Moh Monish <mohdmonish8882@gmail.com>
2025-02-12 06:00:38 +0000

    changes for SO

--- VehicleInspection/app/vehicleinspection/webapp/modone/controller/SalesOrder.controller.js
+++ VehicleInspection/app/vehicleinspection/webapp/modone/controller/SalesOrder.controller.js
@@ -5,51 +5,54 @@ sap.ui.define([
 	"user strict"
 
 	return genericentryform.extend("modonecontroller.SalesOrder", {
+		
 		constructor: function () {
+
 		},
 
 		onInit: async function () {
+
 			genericentryform.prototype.onInit.apply(this, arguments);
+
 		},
+
 		onBeforeShow: async function (oEvent) {
+
 			//this.identifyFormMode(oEvent);
 			this.initialize();
 			this.setEntryFormDataSourceURLForEditMode('');
 			await this.showEntryForm();
 
 		},
+
 		initialize: async function () {
-			RoleInfo = this.getRoleDetails();
-			LoginInfo = this.getLoginInfo();
-			formMode = this.getFormMode();
+
 			this.setPageId('salesOrderPage');
 			this.setFormTitle('');
-
 			this.setBackwardRoute('RouteADMobility');
-
 			this.setEntryFormDataSourceURLForNewMode('');
-
 			this.setEntryFormDataSourceURLToAddData('');
 			this.setEntryFormDataSourceURLToUpdateData('');
-
 			let oModel = new sap.ui.model.json.JSONModel();
 			oModel.loadData("/modone/model/SalesOrderModel.json");
 			this.getView().setModel(oModel, this.getEntryFormDataSourceModelName());
-		},
-		
-		backToLanding: function () {
-			const oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("RouteADMobility", {}, true);
-		},
-		getFullWidth: function () {
-			return new sap.m.FlexItemData({ growFactor: 1 });
-		},
-		onBtnPressExit: function () {
-			const oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("RouteADMobility", {}, true);
+
 		},
 
-		_initialData: {
+		getFullWidth: function () {
+
+			return new sap.m.FlexItemData({ growFactor: 1 });
+
+		},
+
+		onBtnPressExit: function () {
+
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("RouteADMobility", {}, true);
+
+		},
+
+		getInitialData: {
 			columns: [{
 				visible: true,
 				name: "key1",
@@ -68,30 +71,28 @@ sap.ui.define([
 			]
 		},
 
-		_setInitialData: function () {
+		setInitialData: function () {
+
 			const oView = this.getView();
-
 			const oSelectionPanel = oView.byId("columnsPanel");
-
-
-
-			oSelectionPanel.setP13nData(this._initialData.columns);
-
+			oSelectionPanel.setP13nData(this.getInitialData.columns);
 
 		},
 
 		onBtnPressPaymentDialog: function (oEvt) {
+
 			const oView = this.getView();
 			const oPopup = oView.byId("p13nPopup");
 			if (!this._bIsOpen) {
-				this._setInitialData();
+				this.setInitialData();
 				this._bIsOpen = true;
 			}
-
 			oPopup.open(oEvt.getSource());
+
 		},
 
-		onClose: function (oEvt) {
+		onBtnPressClose: function (oEvt) {
+
 			const sReason = oEvt.getParameter("reason");
 			MessageToast.show("Payment Successfully: " + sReason);
 			const oRouter = this.getOwnerComponent().getRouter();
@@ -99,23 +100,19 @@ sap.ui.define([
 
 		},
 
-
-
 		parseP13nState: function (oEvt) {
 
 			if (oEvt) {
 				MessageToast.show("P13n panel change reason:" + oEvt.getParameter("reason"));
 			}
-
 			const oView = this.getView();
 			const oEditor = oView.byId("p13nEditor");
-
 			const oP13nState = {
 				columns: oView.byId("columnsPanel").getP13nData(),
-
 			};
-
 			oEditor.setValue(JSON.stringify(oP13nState, null, '  '));
+
 		}
+
 	})
 }) 
